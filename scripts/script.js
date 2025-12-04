bab0374837b9962835d79fcd5b1e2d3e0d49bcb6
Acach Gabriel Bruno <acachbruno@gmail.com>
2025-12-04 17:32:56 +0300

    Updated index.html

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,5 @@ profileImg.addEventListener('click', () => {
   profileImg.classList.toggle('zoomed');
 });
 
-// Optional: close zoom with ESC key
-document.addEventListener('keydown', (e) => {
-  if (e.key === "Escape" && profileImg.classList.contains('zoomed')) {
-    profileImg.classList.remove('zoomed');
-  }
-});
 
 
